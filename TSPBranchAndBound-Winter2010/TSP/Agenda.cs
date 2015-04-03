d2f843dc75bf81f8142fa3af6eb0dd753cffc46e
Brynne May Makana Onala Hansen <brynnemh@halberd.cs.byu.edu>
2015-04-02 19:56:56 -0600

    initial commit

---
+++ TSPBranchAndBound-Winter2010/TSP/Agenda.cs
@@ -0,0 +1,15 @@
+﻿using Priority_Queue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSP
+{
+    class Agenda
+    {
+        Dictionary<int, HeapPriorityQueue<State>> statespace = new Dictionary<int, HeapPriorityQueue<State>>();
+
+    }
+}
